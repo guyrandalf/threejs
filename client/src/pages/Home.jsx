1b2bfe19e340dcc430e9fa6269d2310a7f147bee
Osabuohien Randalf Ehigiator <107191688+guyrandalf@users.noreply.github.com>
2023-06-26 17:31:54 +0100

    Setup completed

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import { headContainerAnimation, headTextAnimation, headContentAnimation, slideAnimation } from "../config/motion";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
